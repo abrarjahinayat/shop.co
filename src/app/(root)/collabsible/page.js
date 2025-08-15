235e9e22841c7bf8d4c605acc3487b10d1cc4fae
Abrar Jahin Ayat <abrarjahinayat123@gmail.com>
2025-08-15 22:14:34 +0600

    category collasible added

---
+++ src/app/(root)/collabsible/page.js
@@ -0,0 +1,13 @@
+
+import Casual2 from '@/app/components/Casual2'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> 
+  <Casual2/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/components/Casual2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+export default function Casual2() {
+  return (
+    <div>
+<Collapsible>
+  <CollapsibleTrigger>Can I use this in my project?</CollapsibleTrigger>
+  <CollapsibleContent>
+    Yes. Free to use for personal and commercial projects. No attribution
+    required.
+  </CollapsibleContent>
+</Collapsible>
+    </div>
+  )
+}
